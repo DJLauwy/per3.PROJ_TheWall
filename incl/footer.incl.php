072852bbab08c2556ff8ba851aa60f00a29720c9
Lauwy de Hoop <55271928+DJLauwy@users.noreply.github.com>
2020-04-14 16:45:39 +0200

    Add files via upload

--- incl/footer.incl.php
+++ incl/footer.incl.php
@@ -1,8 +1,8 @@
 		<footer>
 			<div>
-				<a href="https://www.facebook.com/officialdjlauwy/"><img src="icon/fb.svg" height="20px" width="20px"></a>
-				<a href="https://www.instagram.com/DJLauwy/"><img src="icon/ig.svg" height="20px" width="20px"></a>
-				<a href="https://twitter.com/DJLauwy"><img src="icon/twi.svg" height="20px" width="20px"></a>
+				<a href="https://www.facebook.com/officialdjlauwy/" target="_blank"><img src="icon/fb.svg" height="20px" width="20px"></a>
+				<a href="https://www.instagram.com/DJLauwy/" target="_blank"><img src="icon/ig.svg" height="20px" width="20px"></a>
+				<a href="https://twitter.com/DJLauwy" target="_blank"><img src="icon/twi.svg"  height="20px" width="20px"></a>
 				<a href="https://open.spotify.com/artist/7KCAeHLa26CpHopAb8sLh7?si=mIChf9qiRLCRta-yw_E4fw" target="_blank"><img src="icon/spot.svg" height="20px" width="20px"></a>
 			</div>
 			<div>
